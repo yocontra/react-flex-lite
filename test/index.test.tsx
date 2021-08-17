c2a31e4dbb8eaa048e3ab8da37e189d815df4dcd
contra <yo@contra.io>
2021-08-17 12:42:19 -0700

    chore: more tests

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -4,18 +4,28 @@ import '@testing-library/jest-dom/extend-expect'
 
 describe('<Flex />', () => {
   it('should render div with correct text and class', () => {
-    const { queryByText } = render(<Flex className="flex">flex text</Flex>)
+    const { queryByText } = render(
+      <Flex className="flex" mt={1}>
+        flex text
+      </Flex>
+    )
     const flexEl = queryByText('flex text')
     expect(flexEl).toBeInTheDocument()
     expect(flexEl).toHaveClass('flex')
+    expect(flexEl).toHaveStyle({ marginTop: 8 })
   })
 })
 
 describe('<Box />', () => {
   it('should render div with correct text and class', () => {
-    const { queryByText } = render(<Box className="box">box text</Box>)
+    const { queryByText } = render(
+      <Box className="box" mt={1}>
+        box text
+      </Box>
+    )
     const boxEl = queryByText('box text')
     expect(boxEl).toBeInTheDocument()
     expect(boxEl).toHaveClass('box')
+    expect(boxEl).toHaveStyle({ marginTop: 8 })
   })
 })
